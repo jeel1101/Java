846eb0c1ea3b03e8851c1380598edefef122b1e3
Patel Jeel <s22cpjeel107@gmail.com>
2024-10-13 17:32:51 +0530

    Literals

---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    int a= 101;
+    float b= 10.1f;
+    char ch= 'A';
+    long d= 5666666666L;
+    
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(ch);
+    System.out.println(d);
+  }
+}
